1a299b6dc0a29f97ed05c4f8a93d523f403c5c83
Faran Ahmad <fakhan.cs@gmail.com>
2024-02-29 13:27:10 +0000

    NbaPlayers component

---
+++ hello-world/src/app/nba-players-component/nba-players-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nba-players-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './nba-players-component.component.html',
+  styleUrl: './nba-players-component.component.css'
+})
+export class NbaPlayersComponentComponent {
+
+}
