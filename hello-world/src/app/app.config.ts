de2890e20ff674e0b915445608a3f2eaff225709
Faran Ahmad <fakhan.cs@gmail.com>
2024-03-02 02:20:06 +0000

    injected HttpClient

--- hello-world/src/app/app.config.ts
+++ hello-world/src/app/app.config.ts
@@ -1,7 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [importProvidersFrom(HttpClientModule)],
+  providers: [provideRouter(routes), provideHttpClient(withFetch())],
 };
