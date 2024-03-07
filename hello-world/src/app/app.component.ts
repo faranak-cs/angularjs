47feb60ff15671483c78af8014bf279e11778298
Faran Ahmad <fakhan.cs@gmail.com>
2024-03-07 23:21:47 +0000

    configured routes

--- hello-world/src/app/app.component.ts
+++ hello-world/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { PlayersComponent } from './players/players.component';
 import { NbaPlayersComponent } from './nba-players/nba-players.component';
 import { MyButtonComponent } from './my-button/my-button.component';
@@ -7,13 +7,14 @@ import { ClickedButtonComponent } from './clicked-button/clicked-button.componen
 import { EmployeeComponent } from './employee/employee.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { FavouriteComponent } from './favourite/favourite.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
 
-// metadata of component
+// decorator with component meta-data
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet,
     PlayersComponent,
     NbaPlayersComponent,
     MyButtonComponent,
@@ -21,6 +22,11 @@ import { FavouriteComponent } from './favourite/favourite.component';
     EmployeeComponent,
     ContactFormComponent,
     FavouriteComponent,
+    NavComponent,
+    HomeComponent,
+
+    RouterLink,
+    RouterOutlet,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
--- hello-world/src/app/app.config.ts
+++ hello-world/src/app/app.config.ts
@@ -5,5 +5,6 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
+  // import the routes array from /app.routes
   providers: [provideRouter(routes), provideHttpClient(withFetch())],
 };
