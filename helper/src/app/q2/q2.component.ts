4268f38930115031d2f27da546ac03dbf65b1a24
Faran Ahmad <fakhan.cs@gmail.com>
2024-03-14 12:47:41 +0000

    Q2

--- helper/src/app/q2/q2.component.ts
+++ helper/src/app/q2/q2.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { User } from '../user';
+import { RestService } from '../rest.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-q2',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './q2.component.html',
-  styleUrl: './q2.component.css'
+  styleUrl: './q2.component.css',
 })
 export class Q2Component {
+  users: User[] = [];
+  usersFiltered: User[] = [];
+  sliderValue: number = 10;
 
+  restService = inject(RestService);
+
+  constructor() {
+    this.restService.getUsers().subscribe((res) => {
+      this.users = res;
+      this.filterUsers(this.sliderValue);
+    });
+  }
+
+  filterUsers(value: number): void {
+    this.usersFiltered = this.users.filter((item) => item.rank <= value);
+  }
 }
--- helper/src/app/user.ts
+++ helper/src/app/user.ts
@@ -1,4 +1,9 @@
 export interface User {
   id: number;
   name: string;
+  username: string;
+  email: string;
+  city: string;
+  company: string;
+  rank: number;
 }
